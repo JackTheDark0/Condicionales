c0f2e319b46bfcbff974bf8b305cbadb7af6b80c
JackTheDark0 <99160661+JackTheDark0@users.noreply.github.com>
2022-05-11 19:39:05 -0500

    Add files via upload

---
+++ Conversion a Celcius.js
@@ -0,0 +1,4 @@
+
+var grado = prompt("Ingrese un valor de grados Farenheit: ")
+var Resultado= ((grado-32)/1.8);
+alert("Los "+ grado +"°F a °C son: "+ Resultado);
---
+++ Hiciste tus tareas.js
@@ -0,0 +1,10 @@
+var Hacer = prompt("Ya hiciste tus tareas? ")
+
+if ( Hacer=="si" || Hacer=="SI") {
+    alert("¡Felicidades, sigue asi uwu!");
+}
+ else if (Hacer=="NO" || Hacer=="no") {
+     alert(" Ponte a hacerlas y hechale ganas mijo xd");
+ } else {
+    alert("Tu respuesta no es indentificada");
+ } 
